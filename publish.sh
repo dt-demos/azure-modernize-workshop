bb8ac1bee03bd5f72e1cbab570a97064f6293c3c
rob jahn <robjahn@gmail.com>
2020-06-19 11:53:33 -0400

    adjust to use date in hugo template

--- publish.sh
+++ publish.sh
@@ -5,13 +5,10 @@ echo "-----------------------------"
 echo "Publishing site content"
 echo "-----------------------------"
 
-echo "Step 1: Update date"
-sed -i "" 's/Last Updated:.*/Last Updated: '"$(date)"'/g' ./layouts/partials/menu-footer.html
-
-echo "Step 2: Compile Site"
+echo "Step 1: Compile Site"
 hugo -c ./content
 
-echo "Step 3: Sync to S3"
+echo "Step 2: Sync to S3"
 aws s3 sync public/ s3://azure-modernize-workshop.alliances.dynatracelabs.com 
 
 echo "-----------------------------"
